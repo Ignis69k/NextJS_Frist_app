5db91fbe5daf4a0d64e82109cb20ed744a948249
owo <tempmail@gmail.com>
2026-06-21 11:44:03 +0700

    add name & id

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 export default function Page() {
   return (
-    <div>Hello World from /app</div>
+    <div>
+      Hello World from /app
+      <br />
+      ID : 6852D10018
+      <br />
+      Name : Patompong Chansomboon
+    </div>
   );
 }
